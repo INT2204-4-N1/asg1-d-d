eea90b83e63c71ebaff79f8a3b43b4278a0cb9d9
duongds <buiduong76021@gmail.com>
2018-11-18 12:56:33 +0700

    bomber run

--- bomberman-starter-starter-project-1/src/uet/oop/bomberman/entities/character/Bomber.java
+++ bomberman-starter-starter-project-1/src/uet/oop/bomberman/entities/character/Bomber.java
@@ -4,9 +4,12 @@ import uet.oop.bomberman.Board;
 import uet.oop.bomberman.Game;
 import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.bomb.Bomb;
+import uet.oop.bomberman.entities.bomb.Flame;
+import uet.oop.bomberman.entities.character.enemy.Enemy;
 import uet.oop.bomberman.graphics.Screen;
 import uet.oop.bomberman.graphics.Sprite;
 import uet.oop.bomberman.input.Keyboard;
+import uet.oop.bomberman.level.Coordinates;
 
 import java.util.Iterator;
 import java.util.List;
@@ -73,10 +76,22 @@ public class Bomber extends Character {
         // TODO: _timeBetweenPutBombs dùng để ngăn chặn Bomber đặt 2 Bomb cùng tại 1 vị trí trong 1 khoảng thời gian quá ngắn
         // TODO: nếu 3 điều kiện trên thỏa mãn thì thực hiện đặt bom bằng placeBomb()
         // TODO: sau khi đặt, nhớ giảm số lượng Bomb Rate và reset _timeBetweenPutBombs về 0
+        if(_input.space && Game.getBombRate() > 0 && _timeBetweenPutBombs < 0) {
+
+            int xt = Coordinates.pixelToTile(_x + _sprite.getSize() / 2);
+            int yt = Coordinates.pixelToTile( (_y + _sprite.getSize() / 2) - _sprite.getSize() ); //subtract half player height and minus 1 y position
+
+            placeBomb(xt,yt);
+            Game.addBombRate(-1);
+
+            _timeBetweenPutBombs = 30;
+        }
     }
 
     protected void placeBomb(int x, int y) {
         // TODO: thực hiện tạo đối tượng bom, đặt vào vị trí (x, y)
+        Bomb b = new Bomb(x, y, _board);
+        _board.addBomb(b);
     }
 
     private void clearBombs() {
@@ -176,6 +191,15 @@ public class Bomber extends Character {
         // TODO: xử lý va chạm với Flame
 
         // TODO: xử lý va chạm với Enemy
+        if(e instanceof Flame) {
+            kill();
+            return false;
+        }
+
+        if(e instanceof Enemy) {
+            kill();
+            return true;
+        }
 
         return true;
     }
