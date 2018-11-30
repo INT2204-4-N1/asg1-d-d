f60a27e6f194554dfc516da93d0cd51f311daf3e
duongds <buiduong76021@gmail.com>
2018-11-30 13:05:31 +0700

    bomber run

--- bomberman-starter-starter-project-1/src/uet/oop/bomberman/entities/tile/Portal.java
+++ bomberman-starter-starter-project-1/src/uet/oop/bomberman/entities/tile/Portal.java
@@ -1,17 +1,32 @@
 package uet.oop.bomberman.entities.tile;
 
+import uet.oop.bomberman.Board;
 import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.character.Bomber;
 import uet.oop.bomberman.graphics.Sprite;
 
 public class Portal extends Tile {
-
-	public Portal(int x, int y, Sprite sprite) {
+    protected Board board;
+	public Portal(int x, int y,Board ds, Sprite sprite) {
 		super(x, y, sprite);
+		board =ds;
 	}
 	
 	@Override
 	public boolean collide(Entity e) {
 		// TODO: xử lý khi Bomber đi vào
+		if(e instanceof Bomber) {
+
+			if (board.detectNoEnemies() == false)
+				return false;
+
+			if (e.getXTile() == getX() && e.getYTile() == getY()) {
+				if (board.detectNoEnemies())
+					board.endGame();
+			}
+
+			return true;
+		}
 		return false;
 	}
 
